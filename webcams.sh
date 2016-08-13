e9904eb239790934fef3b1d85b17e24a57ffa209
khannover <kai.hannover@gmx.de>
2016-08-14 00:16:10 +0200

    scripts added

--- webcams.sh
+++ webcams.sh
@@ -4,6 +4,14 @@ log=/home/pi/scripts/webcams/log.txt
 currentMin=`date +%M`
 configs=`ls /home/pi/scripts/webcams/*.cfg`
 
+if [ $# -gt 0 ]; then
+   htmlPath=$1
+   htmlFile=$2
+   
+else
+   htmlPath=""
+fi
+
 function log(){
   echo "`date`: $1" >> $log
 }
@@ -30,10 +38,19 @@ for c in $configs; do
   done < $c
 done
 
-cd /wwwroot/webcams/
-echo "" > /wwwroot/webcams/index.html
-for d in `find . -mindepth 1 -type d`; do
-  for i in `ls -1 $d/*timelapse*.mp4`; do
-    echo "<a href=$i>$i</a><br>" >> /wwwroot/webcams/index.html
+if [ "$htmlPath" != "" ]; then
+  cd $htmlPath
+  echo "" > $htmlFile
+  for d in `find . -mindepth 1 -type d`; do
+    for i in `ls -1 $d/*timelapse*.mp4`; do
+      echo "<a href=$i>$i</a><br>" >> $htmlFile
+      echo '
+  <video width="320" height="240" controls>
+    <source src="'$i'" type="video/mp4">
+  Your browser does not support the video tag.
+  </video><br><br>
+  ' >> $htmlFile
+    done
   done
-done
+fi
+exit
