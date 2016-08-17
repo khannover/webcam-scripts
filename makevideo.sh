c69b9e11dcb0fd8fdded6a0b3c1ce8c2545a358b
khannover <kai.hannover@gmx.de>
2016-08-17 14:08:16 +0200

    scripts fixed

--- makevideo.sh
+++ makevideo.sh
@@ -7,11 +7,12 @@ function log(){
   echo "`date`: $1" >> $log
 }
 
+today=`date +%y%m%d`
 for d in `find $folders -mindepth 1 -type d`; do
   dates=`ls -1 $d/*.jpg | awk -F"-" '{print $2}' | awk -F"." '{print $1}' | sort -u`
   log "dates found: $dates"
   for dat in $dates; do
-    if [ ! -f $d/`basename $d`-timelapse-${dat}.mp4 ]; then
+    if [ ! -f $d/`basename $d`-timelapse-${dat}.mp4 -a "$dat" != "$today" ]; then
       log "processing $d/`basename $d`-timelapse-$dat.mp4"
       count=1
       for f in `ls $d/*${dat}*.jpg`; do
